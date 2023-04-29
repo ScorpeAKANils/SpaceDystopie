ef1315e610ecf94333424e8d4e1275eb90377971
ScorpeAKANils <90929557+ScorpeAKANils@users.noreply.github.com>
2023-04-29 14:53:55 +0200

    new level test

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -211,6 +211,7 @@ public class Movement : MonoBehaviour
             {
                 isLeaning = true;
                 playerView.transform.rotation = leftLean.rotation;
+                playerView.transform.position = leftLean.position;
             }
 
         }
@@ -226,6 +227,7 @@ public class Movement : MonoBehaviour
             {
                 isLeaning = true;
                 playerView.transform.rotation = rightLean.rotation;
+                playerView.transform.position = rightLean.position;
             }
         }
         //stopp lehnen 
